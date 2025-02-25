ff9e71f423ad185b2889590628d2cdf69bcaa3f6
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-25 21:11:01 +0330

    🐛 Fix core manager

--- core/managers.py
+++ core/managers.py
@@ -10,21 +10,25 @@ class UserManager(BaseManager):
 
         user = self.model(phone_number=phone_number, **kwargs)
 
-        if password:
-            user.set_password(password)
-
-        else:
-            user.set_unusable_password()
+        user.set_password(
+            password) if password else user.set_unusable_password()
 
         user.save(using=self._db)
+
         return user
 
-    def create_superuser(self, phone_number, email, password=None, **kwargs):
-        kwargs.setdefault('is_active', True)
-        kwargs.setdefault('is_staff', True)
-        kwargs.setdefault('is_superuser', True)
+    def create_superuser(self, phone_number, email=None, password=None, **kwargs):
+            kwargs.setdefault('is_active', True)
+            kwargs.setdefault('is_staff', True)
+            kwargs.setdefault('is_superuser', True)
 
-        if not password:
-            raise ValueError('Superusers must have a password')
+            if not password:
+                raise ValueError('Superusers must have a password')
+            # Optionally, enforce that an email must be provided:
+            # if not email:
+            #     raise ValueError('Superusers must have an email address')
 
-        return self.create_user(phone_number=phone_number, email=email, password=password, **kwargs)
+            user = self.model(phone_number=phone_number, email=email, **kwargs)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
